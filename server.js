8244bffc993c70e47c287b14f92aee2ae351cc8a
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-04-20 16:22:04 -0500

    try body parser

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 // cors
 app.use(cors());
+app.use(express.json());
 // make public static
 app.use("/public", express.static(process.cwd() + "/public")); //make public static
 
