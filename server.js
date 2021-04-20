98e903b7aa73786d9c376306ebd8fcc24ef701e6
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-04-20 08:05:19 -0500

    install express and other dependencies, write server logic

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const multiparty = require("multiparty");
+require("dotenv").config();
+
+// instantiate an express app
+const app = express();
+
+const transporter = nodemailer.createTransport({
+  service: "smtp.gmail.com",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+
+// verify connection configuration
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
+app.post("/send", (req, res) => {
+  let form = new multiparty.Form();
+  let data = {};
+  form.parse(req, (err, fields) => {
+    console.log(fields);
+    Object.keys(fields).forEach((property) => {
+      data[property] = fields[property].toString();
+    });
+
+    const mail = {
+      from: data.name,
+      to: process.env.EMAIL,
+      subject: data.subject,
+      text: `${data.name} <${data.email}> \n${data.message}`,
+    };
+
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Something went wrong.");
+      } else {
+        res.status(200).send("Email successfully sent to recipient!");
+      }
+    });
+  });
+});
+
+// make the contact page the first page on the app
+app.route("/").get((req, res) => {
+  res.sendFile(`${process.cwd()}/public/index.html`);
+});
+
+// port will be 5000 for testing
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
